e4736e4d0d2fc9a0860d7b2463c69820de6aac33
Arhab0 <arhabumer2@gmail.com>
2026-07-02 15:47:42 +0500

    nullable fields updated

--- Dto/AttendanceDto.cs
+++ Dto/AttendanceDto.cs
@@ -3,15 +3,15 @@
     public class AttendanceDto
     {
         public int? sid { get; set; }
-        public string TimeInLat { get; set; }
-        public string TimeInLon { get; set; }
-        public string TimeInLocationName { get; set; }
+        public string? TimeInLat { get; set; }
+        public string? TimeInLon { get; set; }
+        public string? TimeInLocationName { get; set; }
         public DateTime? TimeInAt { get; set; }
         public DateTime? TimeInMobile { get; set; }
         public DateTime? TimeInImage { get; set; }
-        public string TimeInBy { get; set; }
-        public string TimeInType { get; set; }
-        public IFormFile AttendanceInImage { get; set; }
+        public string? TimeInBy { get; set; }
+        public string? TimeInType { get; set; }
+        public IFormFile? AttendanceInImage { get; set; }
         public string? TimeOutLat { get; set; }
         public string? TimeOutLon { get; set; }
         public string? TimeOutLocationName { get; set; }
