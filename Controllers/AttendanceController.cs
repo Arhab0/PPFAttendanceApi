56cd1e1dd924df57fca2575dec82309233c0e511
Arhab0 <arhabumer2@gmail.com>
2026-07-13 17:02:47 +0500

    attendance api updated

--- Controllers/AttendanceController.cs
+++ Controllers/AttendanceController.cs
@@ -317,6 +317,14 @@ namespace PPFAttendanceApi.Controllers
 
                 if (dto.Action == "TimeIn")
                 {
+                    var date_check = dto.TimeInAt ?? dto.TimeInMobile ?? dto.TimeInImage;
+
+                    var attendanceCheck = await db.AttendanceLogs.Where(x => (x.EmployeeId == dto.sid || x.UserId == dto.sid) && date_check == (x.TimeInAt ?? x.TimeInMobile ?? x.TimeInImage)!.Value.Date).FirstOrDefaultAsync();
+                    if (attendanceCheck != null)
+                    {
+                        return BadRequest(new { statusCode = 400, message = "Attendance already marked for today." });
+                    }
+
                     if (openSession != null)
                     {
                         return BadRequest(new
