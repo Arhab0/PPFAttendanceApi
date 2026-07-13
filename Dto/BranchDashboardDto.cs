e9db7ad4203017dfa283cb3e45bbaf31e07e5972
Arhab0 <arhabumer2@gmail.com>
2026-07-13 17:16:33 +0500

    api refactored

---
+++ Dto/BranchDashboardDto.cs
@@ -0,0 +1,12 @@
+﻿namespace PPFAttendanceApi.Dto
+{
+    public class BranchDashboardDto
+    {
+        public int BranchId { get; set; }
+        public string BranchName { get; set; }
+        public int TotalStaffCount { get; set; }
+        public int PresentTodayCount { get; set; }
+        public int CheckInCount { get; set; }
+        public int CheckOutCount { get; set; }
+    }
+}
